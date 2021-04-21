417a3ced6fa751178a82ab9a9fb1245d8d68bd91
Jordan Shatford <jordanshatford@live.com>
2021-04-20 21:30:03 -0300

    Added Flask Restful for rest api for our application.
    Updated search to be an api resource. Updated static file endpoints
    to be less complicated. I will need to clean up the file structure of
    the server before it becomes too complicated.

--- server/app.py
+++ server/app.py
@@ -1,53 +1,43 @@
 from flask import jsonify, Flask, send_from_directory, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_restful import Resource, Api
 from youtube_dl import YoutubeDL
 
 app = Flask(__name__)
+api = Api(app, prefix="/api")
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 app.config["SECRET_KEY"] = "TODO_SECRET_KEY"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["SESSION_TYPE"] = "sqlalchemy"
-
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
 app.config["SESSION_SQLALCHEMY"] = db
 Session(app)
 
-
-# Path for our main Svelte page
+# Path for our main Svelte pages
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def base(path):
     return send_from_directory("../client/public", "index.html")
 
 # Paths for all the static files (compiled JS/CSS, etc.)
-@app.route("/build/<path:path>")
-def build(path):
-    return send_from_directory("../client/public/build", path)
+@app.route("/<string:folder>/<string:filename>")
+def build(folder, filename):
+    return send_from_directory(f"../client/public/{folder}", filename)
 
-@app.route("/images/<path:path>")
-def images(path):
-    return send_from_directory("../client/public/images", path)
+# Search api for finding youtube videos
+class Search(Resource):
+    def get(self):
+        YDL_OPTIONS = {
+            "noplaylist": True,
+            "quiet": True,
+        }
+        DEFAULT_SEARCH_RESULT_SIZE = 3
+        search_term = request.args.get("term")
+        results_size = request.args.get("results", DEFAULT_SEARCH_RESULT_SIZE)
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            videos = ydl.extract_info(f"ytsearch{results_size}:{search_term}", download=False)["entries"]
+            return jsonify(videos)
 
-@app.route("/global.css")
-def globalCss():
-    return send_from_directory("../client/public", "global.css")
-
-@app.route("/favicon.png")
-def favicon():
-    return send_from_directory("../client/public", "favicon.png")
-
-# Path to search for youtube videos using key terms
-@app.route("/api/search", methods=["GET"])
-def search_video():
-    YDL_OPTIONS = {
-        "noplaylist": True,
-        "quiet": True,
-    }
-    DEFAULT_SEARCH_RESULT_SIZE = 3
-    search_term = request.args.get("term")
-    results_size = request.args.get("results", DEFAULT_SEARCH_RESULT_SIZE)
-    with YoutubeDL(YDL_OPTIONS) as ydl:
-        videos = ydl.extract_info(f"ytsearch{results_size}:{search_term}", download=False)["entries"]
-        return jsonify(videos)
+api.add_resource(Search, '/search')
