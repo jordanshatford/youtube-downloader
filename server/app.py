b813231748eeabae8548907c756d42f3fd86c8fd
Jordan Shatford <jordanshatford@live.com>
2021-05-03 22:53:49 -0300

    Created Dockerfiles for client and server and docker-compose
    Setup dockerfiles as docker-compose for easy setup and use of the
    application. Running `docker-compose up` while run the application
    in production mode.

--- server/app.py
+++ server/app.py
@@ -10,17 +10,18 @@ from resources.search import http as HttpSearchEndpoints
 from resources.session import http as HttpSessionEndpoints
 from utils.managers import session_manager
 
-
-app = Flask(__name__)
-CORS(app)
-
-app.register_blueprint(HttpDownloadsEndpoints, url_prefix="/api")
-app.register_blueprint(HttpSearchEndpoints, url_prefix="/api")
-app.register_blueprint(HttpSessionEndpoints, url_prefix="/api")
-
-
 @atexit.register
 def shutdown_cleanup():
     # Remove all session files
     if os.path.exists(session_manager.session_dir):
         shutil.rmtree(session_manager.session_dir)
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.register_blueprint(HttpDownloadsEndpoints, url_prefix="/api")
+    app.register_blueprint(HttpSearchEndpoints, url_prefix="/api")
+    app.register_blueprint(HttpSessionEndpoints, url_prefix="/api")
+
+    return app
