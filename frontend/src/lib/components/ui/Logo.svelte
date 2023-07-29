731ef389ce6c0147bb5236f6423f4ea7e1f8d2ca
Jordan Shatford <jordanshatford@live.com>
2023-07-29 19:47:04 +1000

    fix(frontend): use <a> tag instead of button for logo
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- frontend/src/lib/components/ui/Logo.svelte
+++ frontend/src/lib/components/ui/Logo.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { RoutePathConstants } from '$lib/utils/route';
 </script>
 
-<button class="focus:outline-none" on:click={() => goto(RoutePathConstants.SEARCH)}>
+<a class="focus:outline-none" href={RoutePathConstants.SEARCH}>
 	<img class="block h-8 w-auto" src="images/logo.png" alt="Logo" />
-</button>
+</a>
