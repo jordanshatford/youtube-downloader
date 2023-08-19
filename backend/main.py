3d47055367f0768b6ebc3ddf82e37bf10c17d040
Jordan Shatford <jordanshatford@live.com>
2023-08-19 10:34:45 +1000

    chore(backend): print if reload is being used when running server
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- backend/main.py
+++ backend/main.py
@@ -61,5 +61,5 @@ if __name__ == '__main__':
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
     dev = os.environ.get('DEV') is not None
-    print(f'Serving on {host}:{port}', flush=True)
+    print(f'Serving on {host}:{port} (reload={dev})', flush=True)
     uvicorn.run('main:app', host=host, port=port, reload=dev)
