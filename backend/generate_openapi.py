364c1142f490c77e6073148ca7870096c76eb45e
Jordan Shatford <jordanshatford@live.com>
2023-08-20 10:43:35 +1000

    chore(backend): append final newline to openapi.json
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- backend/generate_openapi.py
+++ backend/generate_openapi.py
@@ -8,5 +8,6 @@ try:
     print(f'Dumping OpenAPI specification to {OUTPUT_FILENAME}')
     with open(OUTPUT_FILENAME, 'w') as file:
         json.dump(app.openapi(), file, indent=4)
+        file.write('\n')
 except Exception as e:
     raise SystemExit(e)
