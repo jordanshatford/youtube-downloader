eefb51439be48a67f0945c858d17b530fc371912
Jordan Shatford <jordanshatford@live.com>
2023-08-27 09:26:24 +1000

    feat(backend): add endpoint to delete session
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- backend/app/routers/session.py
+++ backend/app/routers/session.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from fastapi import status
 
+from ..dependencies import depends_session_responses
+from ..dependencies import DependsSession
 from ..models import Session
 from ..utils.managers import session_manager
 
@@ -13,3 +16,11 @@ router = APIRouter(
 def get_session() -> Session:
     session_id = session_manager.create()
     return Session(id=session_id)
+
+
+@router.delete(
+    '', status_code=status.HTTP_204_NO_CONTENT,
+    responses=depends_session_responses,
+)
+def delete_session(session: DependsSession) -> None:
+    session_manager.remove(session.id)
