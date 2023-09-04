9685d77dd4028385289470f63823c96fb2d35a1f
Jordan Shatford <jordanshatford@live.com>
2023-09-04 13:39:38 +1000

    feat(config): add config package to store common configs
    Put tailwindcss config there and use it as a preset in web.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			boxShadow: {
+				dark: '0px 2px 4px 0px hsla(0,0%,0%,0.14), 0px 2px 4px 0px hsla(0,0%,0%,0.12), 0px 2px 4px -1px hsla(0,0%,0%,0.2);'
+			}
+		}
+	},
+	variants: {
+		extend: {
+			backgroundColor: [
+				'active',
+				'responsive',
+				'disabled',
+				'dark',
+				'group-hover',
+				'focus-within',
+				'hover',
+				'focus'
+			],
+			boxShadow: ['dark', 'responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+			cursor: ['responsive', 'disabled'],
+			divideColor: ['responsive', 'dark'],
+			opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
+			scale: ['responsive', 'hover', 'focus'],
+			textColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled']
+		}
+	},
+	plugins: []
+};
