c5b604ac3d44f4dbf53ea962a1e20d4236c24bbe
Jordan Shatford <jordanshatford@live.com>
2023-09-04 17:30:24 +1000

    fix(api): typing issues in youtube search code
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/api/app/utils/youtube.py
+++ apps/api/app/utils/youtube.py
@@ -1,6 +1,8 @@
+import json
 from typing import Any
 
 from pydantic import HttpUrl
+from pydantic import parse_obj_as
 from youtubesearchpython import VideosSearch
 
 from ..models import Channel
@@ -9,23 +11,28 @@ from ..models import Video
 
 def search_youtube(term: str, results_size: int) -> list[Video]:
     try:
-        results = VideosSearch(term, limit=int(results_size))
-        videos = results.result()['result']
+        results = VideosSearch(term, limit=int(results_size)).result()
+        if isinstance(results, str):
+            results = json.loads(results)
+        videos = results.get('result', [])
         return [format_search_result(video) for video in videos]
     except KeyError:
         return []
 
 
 def format_search_result(result: dict[str, Any]) -> Video:
+    channel = Channel(
+        name=result['channel']['name'],
+        url=parse_obj_as(HttpUrl, result['channel']['link']),
+        thumbnail=parse_obj_as(
+            HttpUrl, result['channel']['thumbnails'][0]['url'],
+        ),
+    )
     return Video(
         id=result['id'],
-        url=HttpUrl(result['link']),
+        url=parse_obj_as(HttpUrl, result['link']),
         title=result['title'],
         duration=result['duration'],
-        thumbnail=HttpUrl(result['thumbnails'][0]['url']),
-        channel=Channel(
-            name=result['channel']['name'],
-            url=HttpUrl(result['channel']['link']),
-            thumbnail=HttpUrl(result['channel']['thumbnails'][0]['url']),
-        ),
+        thumbnail=parse_obj_as(HttpUrl, result['thumbnails'][0]['url']),
+        channel=channel,
     )
