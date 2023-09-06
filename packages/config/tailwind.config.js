b12f6a77650ee7cb985e054537bd879160d4a308
Jordan Shatford <jordanshatford@live.com>
2023-09-06 16:02:21 +1000

    fix(config): content to look at in tailwind config
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+    './src/**/*.{html,js,svelte,ts}',
+    '../../apps/*/src/**/*.{html,js,svelte,ts}',
+    '../../packages/*/src/**/*.{html,js,svelte,ts}'
+  ],
 	darkMode: 'class',
 	theme: {
 		extend: {
