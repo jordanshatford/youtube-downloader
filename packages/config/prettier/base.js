1771ce3ff5ceb45a1c3d88bdecd39f4747e9cc89
Jordan Shatford <jordanshatford@live.com>
2023-09-11 16:17:48 +1000

    chore(config): seperate prettier config for base and svelte
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ packages/config/prettier/base.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 100
+};
--- packages/config/prettier/svelte.js
+++ packages/config/prettier/svelte.js
@@ -1,9 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'none',
-	printWidth: 100,
+	...require('./base'),
 	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
 	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
 };
