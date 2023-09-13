8c6fddeab61fd91110f6680b8ac8741df3cd5983
Jordan Shatford <jordanshatford@live.com>
2023-09-13 10:24:41 +1000

    feat(ui): update alert component with new style
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- packages/ui/src/lib/components/Alert.svelte
+++ packages/ui/src/lib/components/Alert.svelte
@@ -1,26 +1,82 @@
 <script lang="ts">
-	import type { Variant } from '../types';
-	import { variantMapping } from '../utilities';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { tv, type VariantProps } from 'tailwind-variants';
 	import { Icon } from '../icons';
+	import { toIcon } from '../utilities';
 
-	export let variant: Variant = 'default';
-	export let title: string;
-	export let message: string | undefined = undefined;
+	const alert = tv({
+		slots: {
+			base: 'rounded border-s-4 p-4',
+			icon: 'flex items-center gap-2',
+			title: 'block font-medium',
+			description: 'mt-2 text-sm'
+		},
+		variants: {
+			variant: {
+				error: {
+					base: 'border-red-500 bg-red-50 dark:border-red-600 dark:bg-red-900',
+					icon: 'text-red-800 dark:text-red-100',
+					description: 'text-red-700 dark:text-red-200'
+				},
+				warning: {
+					base: 'border-yellow-500 bg-yellow-50 dark:border-yellow-600 dark:bg-yellow-900',
+					icon: 'text-yellow-800 dark:text-yellow-100',
+					description: 'text-yellow-700 dark:text-yellow-200'
+				},
+				info: {
+					base: 'border-blue-500 bg-blue-50 dark:border-blue-600 dark:bg-blue-900',
+					icon: 'text-blue-800 dark:text-blue-100',
+					description: 'text-blue-700 dark:text-blue-200'
+				},
+				success: {
+					base: 'border-emerald-500 bg-emerald-50 dark:border-emerald-600 dark:bg-emerald-900',
+					icon: 'text-emerald-800 dark:text-emerald-100',
+					description: 'text-emerald-700 dark:text-emerald-200'
+				}
+			}
+		},
+		defaultVariants: {
+			variant: 'info'
+		}
+	});
+
+	interface $$Props extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof alert> {
+		title?: string;
+		description?: string;
+	}
+
+	export let variant: $$Props['variant'] = 'info';
+	export let title: $$Props['title'] = undefined;
+	export let description: $$Props['title'] = undefined;
+
+	const icon = toIcon(variant);
+
+	const {
+		base,
+		icon: iconClass,
+		title: titleClass,
+		description: descriptionClass
+	} = alert({ variant });
 </script>
 
-<div
-	class="{variantMapping[variant]
-		.class} top-0 items-center justify-between rounded-lg px-2 py-2 shadow dark:shadow-dark md:flex"
->
-	<div class="items-center sm:flex">
-		<div class="flex items-end">
-			{#if variantMapping[variant].icon}
-				<Icon src={variantMapping[variant].icon} class="h-5 w-5 {variantMapping[variant].class}" />
-			{/if}
-			<p class="mx-2 font-bold {variantMapping[variant].class}">{title}</p>
-		</div>
-		{#if message}
-			<p class="pl-2 md:pl-0">{message}</p>
+<div {...$$restProps} role="alert" class={base()}>
+	<div class={iconClass()}>
+		{#if icon}
+			<Icon src={icon} theme="solid" class="h-5 w-5" />
 		{/if}
+		<strong class={titleClass()}>
+			<slot>
+				{#if title}
+					{title}
+				{/if}
+			</slot>
+		</strong>
 	</div>
+	<p class={descriptionClass()}>
+		<slot name="description">
+			{#if description}
+				{description}
+			{/if}
+		</slot>
+	</p>
 </div>
