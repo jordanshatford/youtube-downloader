6bdb792a109fbb359040c060fbdd0be7dbfa69c7
Jordan Shatford <jordanshatford@live.com>
2023-09-29 13:26:20 +1000

    fix(web/ui): proper border on input and select elements
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- packages/ui/src/lib/components/Select.svelte
+++ packages/ui/src/lib/components/Select.svelte
@@ -11,7 +11,7 @@
 		{disabled}
 		on:change
 		bind:value
-		class="w-full rounded-lg border-2 border-zinc-200 p-2 text-zinc-600 outline-none dark:border-zinc-600 dark:bg-zinc-800 dark:text-zinc-200 dark:disabled:bg-zinc-600"
+		class="w-full rounded-lg border-2 border-zinc-200 p-2 text-zinc-600 outline-none focus:border-indigo-600 focus:ring-transparent dark:border-zinc-600 dark:bg-zinc-800 dark:text-zinc-200 dark:disabled:bg-zinc-600"
 	>
 		{#each options as option}
 			<option value={option.value} class="py-1">{option.text}</option>
