d5752f60bc8b65a9bc8e0ea3c3c961ed5d3f8953
Jordan Shatford <jordanshatford@live.com>
2023-10-13 11:16:14 +1100

    feat(core): allow getting next set of results for a given search
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- core/ydcore/search/__init__.py
+++ core/ydcore/search/__init__.py
@@ -9,16 +9,20 @@ from .core import VideosSearch
 class YouTubeVideoSearch:
     def __init__(self, term: str):
         self._term = term
+        self._search = VideosSearch(self._term)
 
     def results(self) -> list[Video]:
         try:
-            results = VideosSearch(self._term).result()
+            results = self._search.result()
             if isinstance(results, str):
                 results = json.loads(results)
             videos = results.get('result', [])
             return [self.format_search_result(video) for video in videos]
         except KeyError:
             return []
+
+    def next(self) -> bool:
+        return self._search.next()
 
     def format_search_result(self, result: dict[str, Any]) -> Video:
         duration = result.get('duration', None)
