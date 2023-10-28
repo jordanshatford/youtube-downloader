6fa0d03ce26c50929635e52fa04377b0322b7141
Jordan Shatford <jordanshatford@live.com>
2023-10-28 23:04:34 +1100

    feat(cli): simplify outputting progress by using one line
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/cli/ydcli/utils.py
+++ apps/cli/ydcli/utils.py
@@ -1,21 +1,15 @@
-import questionary
 from ydcore import DownloadState
 from ydcore import VideoWithOptionsAndStatus
 
 
 def on_status_update(update: VideoWithOptionsAndStatus) -> None:
     state = update.status.state
-    if state in [
-        DownloadState.WAITING, DownloadState.ERROR, DownloadState.DONE,
-    ]:
-        questionary.print(state.value)
-    elif state == DownloadState.DOWNLOADING:
-        progress = update.status.progress
-        questionary.print(
-            f'{state.value} - {progress:3.0f}%' if progress else state,
-        )
-    elif state == DownloadState.PROCESSING:
-        postprocessor = update.status.postprocessor
-        questionary.print(
-            f'{state.value} - {postprocessor}' if postprocessor else state,
-        )
+    progress = update.status.progress
+    postprocessor = update.status.postprocessor
+    output: str = state.value
+    if state == DownloadState.DOWNLOADING and progress is not None:
+        output += f' - {progress:3.1f}%'
+    elif state == DownloadState.PROCESSING and postprocessor is not None:
+        output += f' - {postprocessor}'
+    print('\033[K', end='\r')
+    print(output, end='\n' if state == DownloadState.DONE else '\r')
