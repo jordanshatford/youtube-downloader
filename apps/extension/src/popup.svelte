8aa14d0ed05d04281d9ebba864f23a1c145a639f
Jordan Shatford <jordanshatford@live.com>
2023-11-04 13:39:56 +1100

    feat(extension): add tailwindcss using common config
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/extension/src/popup.svelte
+++ apps/extension/src/popup.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
+	import './style.css';
+
+	let prefferred = window?.matchMedia?.('(prefers-color-scheme: dark)')?.matches;
+
 	let currentTab: chrome.tabs.Tab | null = null;
 	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 		currentTab = tabs[0];
 	});
 </script>
 
-<div>
-	<h2 class="text-center">YouTube Downloader</h2>
-	<div class="container">
+<div class="w-48">
+	<h2 class="text-center text-red-800">YouTube Downloader</h2>
+	<div>
 		{#if currentTab}
-			<p>Current Tab: {currentTab.title}</p>
+			<p>Current Tab: {currentTab.title} dark: {prefferred}</p>
 		{/if}
 	</div>
 	<a href="https://youtubedownloader.duckdns.org" target="_blank">Go to site</a>
 </div>
-
-<style>
-	.container {
-		min-width: 470px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 47px;
-	}
-	.text-center {
-		text-align: center;
-	}
-</style>
