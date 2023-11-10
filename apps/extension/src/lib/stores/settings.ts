0b052138e854ac1cce8862289bd71566ceba0cf2
Jordan Shatford <jordanshatford@live.com>
2023-11-10 13:02:11 +1100

    feat(extension): add settings store using browser storage
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ apps/extension/src/lib/stores/settings.ts
@@ -0,0 +1,5 @@
+import { DEFAULT_DOWNLOAD_OPTIONS } from '@yd/client';
+import { writeableStorage } from '~/lib/stores/writeable-storage';
+
+// Store used for managing preferred settings set by the user.
+export const settings = writeableStorage('settings', DEFAULT_DOWNLOAD_OPTIONS);
