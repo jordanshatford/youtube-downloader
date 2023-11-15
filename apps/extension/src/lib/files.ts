38a8a4d60eb499c4b75186803189034b6f84458a
Jordan Shatford <jordanshatford@live.com>
2023-11-15 13:47:45 +1100

    feat(extension): add save as function to extension
    This is similar to web version, but instead uses the downloads
    api and base64 version of the blob to allow downloading from the
    background entrypoint.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ apps/extension/src/lib/files.ts
@@ -0,0 +1,35 @@
+/**
+ * Save a file to the users computer by downloading programatically.
+ * @param blob - the file as a blob.
+ * @param name - the name to save the file as.
+ */
+export async function saveAs(blob: Blob, name: string): Promise<number> {
+	const base64 = await blobToBase64(blob);
+	return await browser.downloads.download({
+		// Blob type and base64 data for download.
+		url: `data:${blob.type};base64,${base64}`,
+		filename: name,
+		saveAs: false,
+		conflictAction: 'uniquify'
+	});
+}
+
+/**
+ * Covert a blob to a base64 string.
+ * @param blob - the blob.
+ * @returns string - the blob as a base64 string.
+ */
+async function blobToBase64(blob: Blob): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		// On error reject with error from FileReader.
+		reader.onerror = () => reject(reader.error);
+		reader.onloadend = () => {
+			const dataUrl = reader.result as string;
+			// Remove "data:mime/type;base64," prefix from data url.
+			const base64 = dataUrl.substring(dataUrl.indexOf(',') + 1);
+			resolve(base64);
+		};
+		reader.readAsDataURL(blob);
+	});
+}
