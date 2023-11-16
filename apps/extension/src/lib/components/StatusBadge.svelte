3ff9821afdc4c758b7e453b008df33d466cda2ca
Jordan Shatford <jordanshatford@live.com>
2023-11-16 16:23:07 +1100

    chore(extension): duplicate status badge component from web
    This duplication will be removed in the future.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ apps/extension/src/lib/components/StatusBadge.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { DownloadState, type DownloadStatus } from '@yd/client';
+	import { Badge, type BadgeVariants, ProgressBar } from '@yd/ui';
+
+	export let status: DownloadStatus;
+
+	const postProcessorLookup: Record<string, string> = {
+		extractaudio: 'EXTRACTING AUDIO',
+		videoconvertor: 'CONVERTING VIDEO',
+		metadata: 'EMBEDDING',
+		embedthumbnail: 'EMBEDDING',
+		embedsubtitle: 'EMBEDDING',
+		movefiles: 'FINALIZING'
+	};
+
+	const lookup: Record<DownloadState, BadgeVariants> = {
+		[DownloadState.WAITING]: {
+			variant: 'warning',
+			loading: true
+		},
+		[DownloadState.DOWNLOADING]: {
+			variant: 'info',
+			loading: true
+		},
+		[DownloadState.PROCESSING]: {
+			variant: 'info',
+			loading: true
+		},
+		[DownloadState.ERROR]: {
+			variant: 'error'
+		},
+		[DownloadState.DONE]: {
+			variant: 'success'
+		}
+	};
+
+	$: text = status.postprocessor
+		? postProcessorLookup[status.postprocessor?.toLowerCase()] ?? status.state
+		: status.state;
+</script>
+
+{#key status.state}
+	<Badge icon {...lookup[status.state]}>
+		{text}
+		{#if status.progress}
+			<ProgressBar class="-ml-7 -mr-2 mt-2" value={status.progress} />
+		{/if}
+	</Badge>
+{/key}
