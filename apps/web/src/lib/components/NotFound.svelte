3b95b617d3d753484cca6be2d46b52ddedd03059
Jordan Shatford <jordanshatford@live.com>
2024-01-08 09:15:34 -0400

    fix(ui): run formatting to fix linting error
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/web/src/lib/components/NotFound.svelte
+++ apps/web/src/lib/components/NotFound.svelte
@@ -10,7 +10,7 @@
 <div class="-mt-16 grid h-screen place-content-center px-4">
 	<div class="text-center">
 		<h1 class="text-7xl font-black text-gray-300 dark:text-gray-600">{title}</h1>
-		<p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+		<p class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
 			{subtitle}
 		</p>
 		<p class="mt-2 text-gray-500 dark:text-gray-400">
