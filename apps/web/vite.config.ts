c88c1e1158fa78f36d4ddd7d104d0f65f2a785aa
Jordan Shatford <jordanshatford@live.com>
2026-06-12 20:06:15 -0300

    chore(web/ui): move svelte config values into vite config
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -5,7 +6,19 @@ import { defineConfig } from 'vite';
 import { version } from './package.json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+			// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+			// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+			adapter: adapter(),
+			alias: {
+				$uilib: '../../packages/ui/src/lib',
+				'$uilib/*': '../../packages/ui/src/lib/*'
+			}
+		})
+	],
 	define: {
 		__APP_VERSION__: JSON.stringify(version)
 	}
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,20 @@
+import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+			// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+			// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+			adapter: adapter(),
+			alias: {
+				$uilib: './src/lib',
+				'$uilib/*': './src/lib/*'
+			}
+		})
+	]
 });
